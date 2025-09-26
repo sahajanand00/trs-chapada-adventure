951971ea396df94c12d36546beb7e19a16e7b7b8
sahajanand00 <sahajanand0@hotmail.com>
2025-09-25 21:13:59 -0300

    muquinho foto

--- src/components/sections/ExpertSection.tsx
+++ src/components/sections/ExpertSection.tsx
@@ -1,4 +1,4 @@
-import {muquinho-Photo} from "@/assets/muquinho-photo.jpg";
+import muquinho-Photo from "@/assets/muquinho-photo.jpg";
 export const ExpertSection = () => {
   return (
     <section className="py-20 bg-background">
